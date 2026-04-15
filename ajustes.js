e307dd2109df729e3dc1e5fa1d261b3d45df9c55
JuanGarDev <fnxbeats@outlook.com>
2026-04-14 20:48:37 -0600

    index

--- ajustes.js
+++ ajustes.js
@@ -13,3 +13,19 @@ setTimeout(() => {
     }, 500);
 
 }, 3000); 
+
+
+const presentacion = document.getElementById('presentacion');
+
+setTimeout(() => { 
+    setTimeout(() => {
+        presentacion.classList.add('animate__animated', 'animate__fadeOut');
+        setTimeout(() => {
+                presentacion.classList.add('hidden');
+                
+                /* Aquí es donde el resto de tu HTML (productos, botones, etc.)
+                   subirá automáticamente porque "presentacion" ya no ocupa espacio.
+                */
+            }, 1000);
+    }, 4000)
+}, 6000);
